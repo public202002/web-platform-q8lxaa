cab4885d715c9facb0a2595702702750dcc80598
public202002 <public202002@gmail.com>
2021-02-23 00:00:30 +0100

    IIFE   ---   2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log('hello!')
+
+function xxx(aa){
+  console.log("hi")
+  setTimeout(aa, 1000, aa)
+}
+
+(function(aa){
+  console.log("hi")
+  setTimeout(aa, 1000, aa)
+}
+)(function(aa){
+  console.log("hi")
+  setTimeout(aa, 1000, aa)
+})
+
+
+// "foo" is a function declaration
+// and is created on entering the context
+ 
+console.log(foo); // function
+ 
+function foo(x) {
+  console.log(x);
+}(1); // and this is just a grouping operator, not a call!
+ 
+(function (x) {
+  console.log(x);
+})(11); // and this is just a grouping operator, not a call!
+
+foo(10); // and this is already a call, 10
